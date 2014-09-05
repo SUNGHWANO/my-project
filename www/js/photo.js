a713078cfbadb33bca49f44b6fbc0f5f059a283a
Administrator <Administrator@MSDN-SPECIAL>
2014-09-05 14:22:09 +0900

    25

--- www/js/photo.js
+++ www/js/photo.js
@@ -1,4 +1,4 @@
 $(document).ready(function() {
 		$("#container").css("height",$(window).height()+"px");
-		$(".slider").phslider({'animation_time':400,'easing':'easeInOutQuint','animation':'slide','title_time':360,'title_easing':'easeInOutBack','controls':true,'autoplay':false});
+		$(".slider").phslider({'animation_time':400,'easing':'easeInOutQuint','animation':'slide','title_time':370,'title_easing':'easeInOutBack','controls':true,'autoplay':false});
 })
