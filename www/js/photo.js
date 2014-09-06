c19a81c825ec1e85450a1ff802f391def1300a51
Administrator <Administrator@MSDN-SPECIAL>
2014-09-06 13:33:59 +0900

    fdg

--- www/js/photo.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(function() {
-		$("#container").css("height",$(window).height()+"px");
-		$(".slider").phslider({'animation_time':700,'easing':'easeInOutQuint','animation':'slide','title_time':400,'title_easing':'easeInOutBack','controls':true,'autoplay':false});
-})
