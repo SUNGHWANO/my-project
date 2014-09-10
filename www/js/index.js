6b59167e89d6402428219900387c7a1e8e9f4ace
Administrator <Administrator@MSDN-SPECIAL>
2014-09-11 06:58:38 +0900

    1

--- www/js/index.js
+++ www/js/index.js
@@ -4,8 +4,10 @@ $(document).ready(function() {
 	$(window).scroll(function() {
 	    if ($(window).scrollTop() == $(document).height() - $(window).height()) {
 	        $('.swiper-slide').css({'overflow':'auto'});
+	        $('.middle').css({'height':'92%'});
 	    }else{
 	    	$('.swiper-slide').css({'overflow':''});
+	    	$('.middle').css({'height':'81%'});
 	    }
 	});
 	
