2650f38543e34ed950f51e362de24c32dd1d2b14
Administrator <Administrator@MSDN-SPECIAL>
2014-09-10 19:41:15 +0900

    1

--- www/js/menulist.js
+++ www/js/menulist.js
@@ -1,12 +1,7 @@
 var mySwiper = new Swiper('.swiper-container',{
-//Enable Scrollbar
 scrollbar: {
 container :'.swiper-scrollbar',
 hide: false,
-draggable: true,
-watchActiveIndex: true,
-onScrollbarDrag: function(){
-console.log('Dragging')
-}
+draggable: false  
 }
 })
