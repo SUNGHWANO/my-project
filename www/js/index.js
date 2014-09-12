1e5779f298c1dcc606fed4b5548c09f8df88998f
Administrator <Administrator@MSDN-SPECIAL>
2014-09-13 00:41:49 +0900

    1

--- www/js/index.js
+++ www/js/index.js
@@ -4,10 +4,8 @@ $(document).ready(function() {
 	$(window).scroll(function() {
 	    if ($(window).scrollTop() == $(document).height() - $(window).height()) {
 	        $('.swiper-slide').css({'overflow':'auto'});    
-	        $('.footer').animate({height: 0});
 	    }else if($(window).scrollTop()){
 	    	$('.swiper-slide').css({'overflow':''});
-	    	$('.footer').animate({height: '8%'});
 	    }
 	});
 	
@@ -16,19 +14,11 @@ $(document).ready(function() {
 /* innerscroll */
 $(document).ready(function() {
 	
-	var lastScroll = 0;
-	$('.swiper-slide').scroll(function() {		
-		var after = $(this).scrollTop();
-		if(after > lastScroll){			
-			lastScroll = after;
-			//$('.footer').animate({height: 0});
-			console.log('down');
-		}else if(after < lastScroll){
-			lastScroll = after;
-			//$('.footer').animate({height: '8%'});
-			console.log('up');
-		}
+	$('.swiper-slide').on("scrollstart",function(){
+		   alert("스크롤 시작");
+		 
 		});
+		
 	
 });
 /* innerscroll */
