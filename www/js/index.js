f2ab37ca5e95128ba8dce5ce5aca8bed8158e67f
Administrator <Administrator@MSDN-SPECIAL>
2014-09-15 00:28:57 +0900

    1

--- www/js/index.js
+++ www/js/index.js
@@ -3,9 +3,11 @@ $(document).ready(function() {
 	$(window).scroll(function() {
 	    if ($(window).scrollTop() == $(document).height() - $(window).height()) {
 	        $('.swiper-slide').css({'overflow':'auto'});    
+	        $(this).css({'overflow':'hidden'}).bind('touchmove', function(e){e.preventDefault()});
 	    }else{
 	    	$('.swiper-slide').css({'overflow':''});   	
 	    	$('.topoption').css({'display':'none'});
+	    	$(this).unbind('touchmove');
 	    }
 	});
 	/* autoscroll */
