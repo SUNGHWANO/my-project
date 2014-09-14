4473dbb501f09b2f1d316f3ae1a9c78d5e1db522
Administrator <Administrator@MSDN-SPECIAL>
2014-09-15 00:37:23 +0900

    1

--- www/js/index.js
+++ www/js/index.js
@@ -3,11 +3,11 @@ $(document).ready(function() {
 	$(window).scroll(function() {
 	    if ($(window).scrollTop() == $(document).height() - $(window).height()) {
 	        $('.swiper-slide').css({'overflow':'auto'});    
-	        $(this).css({'overflow':'hidden'}).bind('touchmove', function(e){e.preventDefault()});
+	        $('body').css({'overflow':'hidden'}).bind('touchmove', function(e){e.preventDefault()});
 	    }else{
 	    	$('.swiper-slide').css({'overflow':''});   	
 	    	$('.topoption').css({'display':'none'});
-	    	$(this).unbind('touchmove');
+	    	$('body').unbind('touchmove');
 	    }
 	});
 	/* autoscroll */
