96284e4770a7f0feb3e4e72f959b2a54b866eb50
Administrator <Administrator@MSDN-SPECIAL>
2014-10-03 11:44:45 +0900

    1

--- www/js/idangerous.swiper.js
+++ www/js/idangerous.swiper.js
@@ -327,14 +327,19 @@ $('.swiper-slide').scroll(function(){
 
 $(window).scroll(function(){
 	
-	if($(this).scrollTop() > scrollFinish){
-					
-		}else if($(this).scrollTop() < scrollFinish){
-			
-			$('.footoption').fadeIn();	
-	
-		}
-		scrollFinish = $(this).scrollTop();
+	if ($(window).scrollTop() == $(document).height() - $(window).height()) {
+        //$('.swiper-slide').css({'overflow':'auto'});    
+		//$('.swiper-scrollbar2').css({'border-bottom':'1px solid #FFFFFF'});
+		$('.swiper-slide').css({'overflow':'auto'});    	
+    }else if ($(window).scrollTop() == 0) {
+    	//$('.swiper-slide').css({'overflow':''});   
+    }else{
+    	//$('.swiper-slide').css({'overflow':''});
+    	//$('.swiper-slide').css({overflow:'hidden'}).bind('touchmove', function(e){e.preventDefault()});
+    	//$('.swiper-scrollbar2').css({'border-bottom':'1px solid #D8D8D8'});
+    	$('.swiper-slide').css({'overflow':''}); 
+    	$('.menubar').fadeIn();
+    }   
 	
 });
 /**/
